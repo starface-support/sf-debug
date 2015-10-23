8a22972ba836ecfbbe796eaa866db81d8e58d11c
Niklas Janz <janz@starface.de>
2015-10-23 10:42:09 +0200

    That's some bad-hat piping, Harry
    * also piping stderr to files
    * changed some echos (also including notice for skipping @ memdump)
    - removed stdout /dev/null pipe for zip

--- debug.sh
+++ debug.sh
@@ -2,8 +2,8 @@
 
 finish() {
 	ARCHIVE="/root/$(mktemp -q -u debuginfo-XXXXXXXX.zip)"
-	echo Finishing up, zipping $FOLDER to $ARCHIVE
 	
+	echo Finishing up, zipping $FOLDER to $ARCHIVE	
     zip -r $ARCHIVE "$FOLDER" "/etc/asterisk/" "/var/log"
 	rm -rf $FOLDER/
 }
@@ -18,45 +18,46 @@ trap finish EXIT
 
 mkdir $AST $APPLIANCE $OS $NET
 
-echo Identifying appliance
-appliance_identify.sh >$APPLIANCE/appliance_identify.txt
-appliance_info.sh check_cards >$APPLIANCE/appliance_cards.txt
+echo Identifying appliance...
+appliance_identify.sh 2>&1>$APPLIANCE/appliance_identify.txt
+appliance_info.sh check_cards 2>&1>$APPLIANCE/appliance_cards.txt
 
-echo Dumping processes
-uptime > $OS/proc.txt
-ps aux >> $OS/proc.txt
+echo Dumping processlist...
+uptime 2>&1>$OS/proc.txt
+ps aux 2>&1>>$OS/proc.txt
 
-echo Networking informations
-lsof -i > $NET/lsof-i.txt
-netstat -tulpen > $NET/netstat-tulpen.txt
-netstat -an > $NET/netstat-an.txt
-ifconfig > $NET/ifconfig.txt
-zip -r $NET/nw-scripts.zip /etc/sysconfig/network-scripts/ >/dev/null
-iptables-save >$NET/iptables-current.txt
+echo Gathering networking informations...
+lsof -i 2>&1>$NET/lsof-i.txt
+netstat -tulpen 2>&1>$NET/netstat-tulpen.txt
+netstat -an 2>&1>$NET/netstat-an.txt
+ifconfig 2>&1>$NET/ifconfig.txt
+zip -r $NET/nw-scripts.zip /etc/sysconfig/network-scripts/
+iptables-save 2>&1>$NET/iptables-current.txt
+route -n 2>&1>$NET/routes.txt
 
-echo Checking SF avaibility
+echo Checking STARFACE HQ avaibility...
 curl -k https://license.starface.de 2>&1> >$NET/https-license.txt
-curl -k http://starface.de 2>&1> >$NET/https-license.txt
+curl http://starface.de 2>&1> >$NET/https-license.txt
 
 echo Poking Asterisk
-asterisk -rx 'module show' > $AST/modules.txt
-asterisk -rx 'sip show peers' > $AST/peers.txt
-asterisk -rx 'sip show registry' > $AST/registry.txt
-asterisk -rx 'sip show channels' > $AST/sip_channels.txt
-asterisk -rx 'core show channels' > $AST/core_channels.txt
+asterisk -rx 'module show' 2>&1>$AST/modules.txt
+asterisk -rx 'sip show peers' 2>&1>$AST/peers.txt
+asterisk -rx 'sip show registry' 2>&1>$AST/registry.txt
+asterisk -rx 'sip show channels' 2>&1>$AST/sip_channels.txt
+asterisk -rx 'core show channels' 2>&1>$AST/core_channels.txt
 
-echo Poking Java
+echo Poking Java... Skip with CTRL + C
 jmap -dump:live,format=b,file=$FOLDER/heap.bin $(jps | grep 'Bootstrap' | awk '{ print $1}')
-jmap -heap $(ps aux | awk '/[j]ava -Djavax/ { print $2 }') > $FOLDER/heap.txt
-jstack $(ps aux | awk '/[j]ava -Djavax/ { print $2 }') > $FOLDER/jstack.txt
+jmap -heap $(ps aux | awk '/[j]ava -Djavax/ { print $2 }') 2>&1>$FOLDER/heap.txt
+jstack $(ps aux | awk '/[j]ava -Djavax/ { print $2 }') 2>&1>$FOLDER/jstack.txt
 
 echo Checking devices
-lspci > $FOLDER/pci.txt
-lsusb > $FOLDER/usb.txt
-df -h > $FOLDER/df.txt
+lspci 2>&1>$FOLDER/pci.txt
+lsusb 2>&1>$FOLDER/usb.txt
+df -h 2>&1>$FOLDER/df.txt
 
 echo Verifying RPMs, this will take some time. Skip with CTRL + C
-rpm -qa > $FOLDER/rpm_qa.txt
-rpm -Va > $FOLDER/rpm_va.txt
+rpm -qa 2>&1>$FOLDER/rpm_qa.txt
+rpm -Va 2>&1>$FOLDER/rpm_va.txt
 
 finish
