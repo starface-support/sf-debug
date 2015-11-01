dffd5c4113b19cfcaa0cdcb83f4a5ef6e809c37c
Niklas Janz <janz@starface.de>
2015-11-01 13:26:44 +0100

    finally functions
    * encased all of the logical steps in seperate bash functions

--- debug.sh
+++ debug.sh
@@ -18,51 +18,70 @@ trap finish EXIT
 
 mkdir $AST $APPLIANCE $OS $NET
 
-echo Identifying appliance...
-appliance_identify.sh 2>&1>$APPLIANCE/appliance_identify.txt
-appliance_info.sh check_cards 2>&1>$APPLIANCE/appliance_cards.txt
+hw-info(){
+	echo Identifying appliance...
+	appliance_identify.sh 2>&1>$APPLIANCE/appliance_identify.txt
+	appliance_info.sh check_cards 2>&1>$APPLIANCE/appliance_cards.txt
 
-echo Dumping processlist...
-uptime 2>&1>$OS/proc.txt
-ps aux 2>&1>>$OS/proc.txt
+	echo Checking devices
+	# ToDo Merge files
+	lspci 2>&1>$APPLIANCE/pci.txt
+	lspci 2>&1>>$APPLIANCE/pci.txt
+	# ToDo Merge files
+	lsusb 2>&1>$APPLIANCE/usb.txt
+	lsusb 2>&1>>$APPLIANCE/usb.txt
+	df -h 2>&1>$APPLIANCE/df.txt
+	lsblk -oNAME,FSTYPE,MOUNTPOINT,TYPE,SIZE 2>&1>$APPLIANCE/lsblk.txt
+}
 
-echo Gathering networking informations...
-lsof -i 2>&1>$NET/lsof-i.txt
-netstat -tulpen 2>&1>$NET/netstat-tulpen.txt
-netstat -an 2>&1>$NET/netstat-an.txt
-ifconfig 2>&1>$NET/ifconfig.txt
-zip -qr $NET/nw-scripts.zip /etc/sysconfig/network-scripts/
-iptables-save 2>&1>$NET/iptables-current.txt
-route -n 2>&1>$NET/routes.txt
+os-details(){
+	echo Dumping processlist...
+	uptime 2>&1>$OS/proc.txt
+	ps aux 2>&1>>$OS/proc.txt
+}
 
-echo Checking STARFACE HQ avaibility...
-curl -k https://license.starface.de 2>&1> $NET/https-license.txt
-curl http://starface.de 2>&1> $NET/https-license.txt
+nw-details(){
+	echo Gathering networking informations...
+	lsof -i 2>&1>$NET/lsof-i.txt
+	netstat -tulpen 2>&1>$NET/netstat-tulpen.txt
+	netstat -an 2>&1>$NET/netstat-an.txt
+	ifconfig 2>&1>$NET/ifconfig.txt
+	zip -qr $NET/nw-scripts.zip /etc/sysconfig/network-scripts/
+	iptables-save 2>&1>$NET/iptables-current.txt
+	route -n 2>&1>$NET/routes.txt
 
-echo Poking Asterisk
-asterisk -rx 'module show' 2>&1>$AST/modules.txt
-asterisk -rx 'sip show peers' 2>&1>$AST/peers.txt
-asterisk -rx 'sip show registry' 2>&1>$AST/registry.txt
-asterisk -rx 'sip show channels' 2>&1>$AST/sip_channels.txt
-asterisk -rx 'core show channels' 2>&1>$AST/core_channels.txt
-asterisk -rx 'pri show spans' 2>&1>$AST/pri_spans.txt
-asterisk -rx 'srx show layers' 2>&1>$AST/srx_layers.txt
+	echo Checking STARFACE HQ avaibility...
+	curl -k https://license.starface.de 2>&1> $NET/https-license.txt
+	curl http://starface.de 2>&1> $NET/https-license.txt
+}
 
-echo Poking Java...
-jmap -dump:live,format=b,file=$FOLDER/heap.bin $(jps | grep 'Bootstrap' | awk '{ print $1}')
-jmap -heap $(ps aux | awk '/[j]ava -Djavax/ { print $2 }') 2>&1>$FOLDER/heap.txt
-jstack $(ps aux | awk '/[j]ava -Djavax/ { print $2 }') 2>&1>$FOLDER/jstack.txt
+ast-details(){
+	echo Poking Asterisk
+	asterisk -rx 'module show' 2>&1>$AST/modules.txt
+	asterisk -rx 'sip show peers' 2>&1>$AST/peers.txt
+	asterisk -rx 'sip show registry' 2>&1>$AST/registry.txt
+	asterisk -rx 'sip show channels' 2>&1>$AST/sip_channels.txt
+	asterisk -rx 'core show channels' 2>&1>$AST/core_channels.txt
+	asterisk -rx 'pri show spans' 2>&1>$AST/pri_spans.txt
+	asterisk -rx 'srx show layers' 2>&1>$AST/srx_layers.txt
+}
 
-echo Checking devices
-# ToDo Merge files
-lspci 2>&1>$APPLIANCE/pci.txt
-lspci 2>&1>>$APPLIANCE/pci.txt
-# ToDo Merge files
-lsusb 2>&1>$APPLIANCE/usb.txt
-lsusb 2>&1>>$APPLIANCE/usb.txt
-df -h 2>&1>$APPLIANCE/df.txt
-lsblk -oNAME,FSTYPE,MOUNTPOINT,TYPE,SIZE 2>&1>$APPLIANCE/lsblk.txt
+java-details(){
+	echo Poking Java...
+	jmap -dump:live,format=b,file=$FOLDER/heap.bin $(jps | grep 'Bootstrap' | awk '{ print $1}')
+	jmap -heap $(ps aux | awk '/[j]ava -Djavax/ { print $2 }') 2>&1>$FOLDER/heap.txt
+	jstack $(ps aux | awk '/[j]ava -Djavax/ { print $2 }') 2>&1>$FOLDER/jstack.txt
+}
 
-echo Verifying RPMs, this will take some time. Skip with CTRL + C
-rpm -qa 2>&1>$FOLDER/rpm_qa.txt
-rpm -Va 2>&1>$FOLDER/rpm_va.txt
+rpm-details(){
+	echo Verifying RPMs, this will take some time. Skip with CTRL + C
+	rpm -qa 2>&1>$FOLDER/rpm_qa.txt
+	rpm -Va 2>&1>$FOLDER/rpm_va.txt
+}
+
+hw-info
+os-details
+nw-details
+ast-details
+java-details
+rpm-details
